44e14af468ada69c51e16c08cdef38974f6b1904
lingfeng179 <junjielingfeng179@163.com>
2018-12-07 17:19:00 +0800

    IDEA github Test

---
+++ src/main/java/com/lingfeng/otn/OtnApplication.java
@@ -0,0 +1,12 @@
+package com.lingfeng.otn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OtnApplication.class, args);
+    }
+}
